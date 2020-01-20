9f57344772e154fb2698963dd7776b4d0f9a0947
shreeniketc <shreeniketc@kpit.com>
2020-01-20 23:53:52 +0530

    doxygen comments added

---
+++ build/MovieDBCore/MovieDataBase/CMakeFiles/movieDbHandler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/movieDbHandler.dir/DBManager.cpp.o"
+  "CMakeFiles/movieDbHandler.dir/MovieDatabaseImpl.cpp.o"
+  "libmovieDbHandler.pdb"
+  "libmovieDbHandler.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/movieDbHandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/MovieDBCore/Sources/CMakeFiles/movieDbSources.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kpit/c++11/proj/MovieDB/MovieDBCore/Sources/SampleSource.cpp" "/home/kpit/c++11/proj/MovieDB/build/MovieDBCore/Sources/CMakeFiles/movieDbSources.dir/SampleSource.cpp.o"
+  "/home/kpit/c++11/proj/MovieDB/MovieDBCore/Sources/SourceProvider.cpp" "/home/kpit/c++11/proj/MovieDB/build/MovieDBCore/Sources/CMakeFiles/movieDbSources.dir/SourceProvider.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../api"
+  "../MovieDBCore"
+  "../MovieDBCore/MovieDataBase"
+  "../MovieDBCore/Sources"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
